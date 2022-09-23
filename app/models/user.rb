bdb7925a973d223911de77920f7213ff9847df7b
Raj Kumar <gpsmartcodes@gmail.com>
2022-09-23 09:16:12 +0800

    Generate orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
   has_secure_password
+
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end
