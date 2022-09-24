cfcb9aabcdef4d1e0064427cdead0a8de7090dce
Raj Kumar <gpsmartcodes@gmail.com>
2022-09-25 00:11:41 +0800

    Decreases the product quantity by the placement quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,13 +7,14 @@ class Order < ApplicationRecord
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
 
-  # @param product_ids_and_quantities [Array<Hash>] something  like this `[{product_id: 1, quantity: 2}]`
+  # @param product_ids_and_quantities [Array<Hash>] something like this `[{product_id: 1, quantity: 2}]`
   # @yield [Placement] placements build
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
-    product_ids_and_quantities.each do |product_id_and_quantity|
-      placement = placements.build(product_id: product_id_and_quantity[:product_id])
-      yield placement if block_given?
-    end
+    product_ids_and_quantities.each do |product_id_and_quantity| placement = placements.build(
+      product_id: product_id_and_quantity[:product_id],
+      quantity: product_id_and_quantity[:quantity],
+    )
+      yield placement if block_given?     end
   end
 
   def set_total!
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_155900) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_160737) do
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
     t.decimal "total"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_155900) do
     t.integer "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["order_id"], name: "index_placements_on_order_id"
     t.index ["product_id"], name: "index_placements_on_product_id"
   end
