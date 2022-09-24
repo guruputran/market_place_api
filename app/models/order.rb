7ce6bfd15aa1a4aaa7e2fa5812f43241cdb16fc0
Raj Kumar <gpsmartcodes@gmail.com>
2022-09-25 00:07:09 +0800

    Allows the order to be placed along with product quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,15 @@ class Order < ApplicationRecord
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
 
+  # @param product_ids_and_quantities [Array<Hash>] something  like this `[{product_id: 1, quantity: 2}]`
+  # @yield [Placement] placements build
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      placement = placements.build(product_id: product_id_and_quantity[:product_id])
+      yield placement if block_given?
+    end
+  end
+
   def set_total!
     self.total = products.sum :price
   end
